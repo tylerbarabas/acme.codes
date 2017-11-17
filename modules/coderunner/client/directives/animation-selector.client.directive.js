16dca2f170cdf3c4cdf04fc1352857e48237d00f
Tyler Barabas <tylerbarabas@gmail.com>
2017-11-17 09:04:39 +0000

    remove domain specific hack from animation selector

--- modules/coderunner/client/directives/animation-selector.client.directive.js
+++ modules/coderunner/client/directives/animation-selector.client.directive.js
@@ -2,25 +2,8 @@
 
 angular.module('coderunner').directive('animationSelector', ['$location','$http','$compile','Api','$rootScope',
 	function(location,$http,$compile,Api,$rootScope) {
-	    // Small hack to have xxx.acme.codes (which are http) and acme.codes (https)
-	    // to use different html files to point correctly at correlating api sub domains.
-	    // This needs to be removed if in future all sub domains are https.
-        var $full_location = location.absUrl();
-        var $fl_parsed = $full_location.split('://');
-        var $sub_doms = $fl_parsed[1].split('.');
-        var $sub_dom;
-//        if ($sub_doms.length >= 3)
-        if ($sub_doms[0] == 'www')
-            {
-            $sub_dom = '';
-            }
-        else
-            {
-            $sub_dom = '-' + $sub_doms[0];
-            };
 		return {
-			templateUrl: ('/modules/coderunner/client/views/animation-selector.client.directive' +
-			              $sub_dom + '.html'),
+			templateUrl: ('/modules/coderunner/client/views/animation-selector.client.directive.html'),
 			restrict: 'E',
 			replace: true,
 			link: function postLink(scope, element, attrs) {
